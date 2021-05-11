9b80b1cafc65b063f2e5577a4ede8a4759b50204
Salman Wiryansyah <71618052+Salman-n@users.noreply.github.com>
2021-05-11 22:23:35 +0700

    Add files via upload

---
+++ Labwork 4/register.php
@@ -0,0 +1,10 @@
+<!--tampilan data akan jalan jika dijalankan di Localhost / xampp-->
+<?php
+
+$Username = $_POST['username'];
+$Pass = $_POST['password'];
+
+echo "Username anda :".$Username;
+echo "Password anda :".$Pass;
+?>
+<br>
